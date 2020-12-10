eb2ab46f373de9310994afc500a54b76bd9dc95b
hirossan4049 <haruto405329@gmail.com>
2020-12-10 20:24:24 +0900

    bug

---
+++ Pods/Target Support Files/Pods-observ-observUITests/Pods-observ-observUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_observ_observUITests : NSObject
+@end
+@implementation PodsDummy_Pods_observ_observUITests
+@end
--- observ/Views/ViewController.swift
+++ observ/Views/ViewController.swift
@@ -6,69 +6,19 @@
 //
 
 import UIKit
-import SafariServices
+import PTCardTabBar
 
-
-class ViewController: UIViewController {
-    private var conductor: HomeConductor!
-    
-    @IBOutlet weak var feedsTableView: UITableView!
-    fileprivate let refreshCtl = UIRefreshControl()
-    
+class ViewController: PTCardTabBarController {
 
     override func viewDidLoad() {
+        let vc1 = HomeViewController()
+        let vc2 =  LabelViewController(title: "test")
+        
+        vc1.tabBarItem = UITabBarItem(title: "Home", image: UIImage(named: "home"), tag: 1)
+        vc2.tabBarItem = UITabBarItem(title: "Fav", image: UIImage(named: "calendar"), tag: 2)
+        
+        self.viewControllers = [vc1, vc2]
+        
         super.viewDidLoad()
-        conductor = HomeConductor()
-        conductor.reload = self.reload
-        
-        feedsTableView.dataSource = self
-        feedsTableView.delegate = self
-        feedsTableView.register(UINib(nibName: "ArticleTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
-        feedsTableView.separatorStyle = .none
-        feedsTableView.refreshControl = refreshCtl
-        
-        refreshCtl.addTarget(self, action: #selector(self.refresh(sender:)), for: .valueChanged)
-        
-        conductor.feedsGet()
-                
     }
-    
-    @objc func refresh(sender: UIRefreshControl){
-        sender.endRefreshing()
-    }
-    
-    func reload(){
-        feedsTableView.reloadData()
-    }
-    
-}
-
-
-extension ViewController: UITableViewDataSource, UITableViewDelegate{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return conductor.feeds.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ArticleTableViewCell
-        cell.titleLabel.text = conductor.feeds[indexPath.row].title
-        cell.descriptionLabel.text = conductor.feeds[indexPath.row].preview
-        cell.lineView.backgroundColor = conductor.feeds[indexPath.row].site.lineColor()
-        
-        cell.imageView?.image = conductor.feeds[indexPath.row].site.getImage(size: cell.logoSize)
-        
-        print(cell.logoSize)
-        print(cell.imageView?.image?.size)
-        
-        cell.selectionStyle = .none
-        return cell
-    }
-
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-         tableView.deselectRow(at: indexPath, animated: true)
-        let safariVC = SFSafariViewController(url: NSURL(string: conductor.feeds[indexPath.row].url)! as URL)
-        present(safariVC, animated: true, completion: nil)
-    }
-    
-    
 }

2927c4bd735ba11cc3c5817f97e461a48aa26185
hirossan4049 <haruto405329@gmail.com>
2020-12-13 15:40:42 +0900

    fix

--- observ/Model/StarModel.swift
+++ observ/Model/StarModel.swift
@@ -13,19 +13,29 @@ class StarModel {
     private let ITEM_KEY = "Stars"
     
     public func fetchStars() -> [Article]?{
+        let jsonArray:[String] = userDefaluts.array(forKey: ITEM_KEY) as! [String]
+        var retArray:[Article] = []
         
-        return userDefaluts.codable(forKey: ITEM_KEY) as? [Article]
-//        let restoredPersons: [Article]? = UserDefaults.standard.codable(forKey: "person")
-
+        for item in jsonArray{
+            let jsonData = item.data(using: .utf8)!
+            retArray.append(try! JSONDecoder().decode(Article.self, from: jsonData))
+        }
+        return retArray
     }
     
     
     public func setStar(article: Article){
-        
+        var jsonArray:[String] = userDefaluts.array(forKey: ITEM_KEY) as! [String]
+        let jsonData = try! JSONEncoder().encode(article)
+        let jsonString = String(data: jsonData, encoding: .utf8)!
+        jsonArray.append(jsonString)
+        userDefaluts.set(jsonArray, forKey: ITEM_KEY)
     }
     
     public func deleteStar(forRow row: Int) {
-        
+        var jsonArray:[String] = userDefaluts.array(forKey: ITEM_KEY) as! [String]
+        jsonArray.remove(at: row)
+        userDefaluts.set(jsonArray, forKey: ITEM_KEY)
     }
     
     

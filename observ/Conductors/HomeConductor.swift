185ff4a413076ddd23c8839f56f8bd069b0836ff
hirossan4049 <haruto405329@gmail.com>
2020-12-20 12:32:21 +0900

    f

--- observ/Conductors/HomeConductor.swift
+++ observ/Conductors/HomeConductor.swift
@@ -10,7 +10,12 @@ import Foundation
 class HomeConductor{
     public var feeds:[Article] = []
     public var reload:(() -> ())?
+    private var starModel: StarModel!
     
+    init() {
+        starModel = StarModel()
+        
+    }
     
     func feedsGet(){
         let getArticles:[Article.SiteType] = [.zenn, .hatena]
@@ -25,6 +30,18 @@ class HomeConductor{
     func reloadFeeds(){
         self.feeds = []
         self.feedsGet()
+    }
+    
+    func add_star(forRow row: Int) {
+        let article:Article = feeds[row]
+        if starModel.getStarIndex(url: article.url) == nil{
+            return
+        }
+//        starModel.setStar(article: article)
+    }
+    func remove_star(forRow row: Int){
+        let index = starModel.getStarIndex(url: feeds[row].url)
+        starModel.removeStar(forRow: index!)
     }
     
     private func feedgetted(article:[Article]){
--- observ/Model/StarModel.swift
+++ observ/Model/StarModel.swift
@@ -7,12 +7,18 @@
 
 import Foundation
 
+protocol StarModelInput {
+    func fetchStars() -> [Article]
+    func addStar(article: Article)
+    func removeStar(forRow: Int)
+    func getStarIndex(url: String) -> Int?
+}
 
-class StarModel {
+final class StarModel:StarModelInput {
     private let userDefaluts = UserDefaults.standard
     private let ITEM_KEY = "Stars"
     
-    public func fetchStars() -> [Article]?{
+    func fetchStars() -> [Article]{
         let jsonArray:[String] = userDefaluts.array(forKey: ITEM_KEY) as! [String]
         var retArray:[Article] = []
         
@@ -24,19 +30,34 @@ class StarModel {
     }
     
     
-    public func setStar(article: Article){
-        var jsonArray:[String] = userDefaluts.array(forKey: ITEM_KEY) as! [String]
+    func addStar(article: Article){
+        var jsonArray:[String] = userDefaluts.array(forKey: ITEM_KEY) as? [String] ?? []
         let jsonData = try! JSONEncoder().encode(article)
         let jsonString = String(data: jsonData, encoding: .utf8)!
         jsonArray.append(jsonString)
         userDefaluts.set(jsonArray, forKey: ITEM_KEY)
     }
     
-    public func deleteStar(forRow row: Int) {
+    func removeStar(forRow row: Int) {
         var jsonArray:[String] = userDefaluts.array(forKey: ITEM_KEY) as! [String]
         jsonArray.remove(at: row)
         userDefaluts.set(jsonArray, forKey: ITEM_KEY)
     }
+    
+    
+    func getStarIndex(url: String) -> Int?{
+        let stars = fetchStars()
+        for (index, item) in stars.enumerated(){
+            if item.url == url{
+                return index
+            }
+        }
+        return nil
+    }
+    
+//    public func getStarIndex(title: String){
+//
+//    }
     
     
 }
